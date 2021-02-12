0bc249b4a782347baea4b376639dff66dad0954e
hakansrc <hakansrc@msn.com>
2021-02-13 02:36:31 +0300

    gereksiz dosyalar kaldırıldı. gonderilmeye hazır

--- index.js
+++
@@ -1,7 +0,0 @@
-const M3U8FileParser = require('m3u8-file-parser');
-const fs = require('fs');
-const content = fs.readFileSync('master.m3u8', { encoding: 'utf-8' });
-
-const reader = new M3U8FileParser();
-reader.read(content);
-console.log(JSON.stringify(reader.getResult(), null, 2));
--- make
+++
@@ -1,2 +0,0 @@
-m3u8_downloader.o: m3u8_downloader.h m3u8_downloader.c
-    gcc -o m3u8_downloader.o  m3u8_downloader.c -lcurl
